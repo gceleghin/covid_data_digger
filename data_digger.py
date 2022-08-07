e7f3418848be0f4e1dee76294dea7cffcce8012b
Giordano <giordano.celeghin@gmail.com>
2022-08-07 20:54:43 +0200

    Refactored code into functions to be imported as a module without running code

--- data_digger.py
+++ data_digger.py
@@ -10,42 +10,22 @@ URL_LATEST_DATA = "https://github.com/pcm-dpc/COVID-19/raw/master/dati-json/dpc-
 URL_ALL_DATA = "https://github.com/pcm-dpc/COVID-19/raw/master/dati-json/dpc-covid19-ita-province.json"
 DATE_BEGINNING_DATA = "2020-02-24"
 
-date_to_check = str(date.today())
-date_format = "%Y-%m-%d"
+#date_to_check = str(date.today())
+DATE_FORMAT = "%Y-%m-%d"
 
-parser = argparse.ArgumentParser(description='Covid data digger')
-parser.add_argument(
-    '--date',
-    type = str,
-    help = "Date you want to get the number of cases from, formatted AAAA-MM-DD"
-)
-parser.add_argument(
-    '--xlsx',
-    action='store_true',
-    help = "Write the results in an .xlsx file"
-)
-parser.add_argument(
-    '--xls',
-    action='store_true',
-    help = "Write the results in an .xls file"
-)
-
-args = parser.parse_args()
-
-if (args.date):
-    try:
-        date_to_check = str(datetime.strptime(args.date, date_format))[:10]
-        if (date_to_check < DATE_BEGINNING_DATA):
-            date_to_check = str(date.today())    
-            print("There is no data available before February 24th 2020, defaulting to today")
-    except ValueError:
-        date_to_check = str(date.today())
-        print("Date format not valid, defaulting to today")
-else:
-    print("No date specified, defaulting to today")
-
-print("Checking statistics for " + date_to_check)
 regions = {}
+
+def dateChecker(day):
+    try:
+        date_to_check = str(datetime.strptime(day, DATE_FORMAT))[:10]
+        if (date_to_check < DATE_BEGINNING_DATA):
+            print("There is no data available before February 24th 2020, defaulting to today")
+            return(str(date.today()))
+        else:
+            return(date_to_check)
+    except ValueError:
+        print("Date format not valid, defaulting to today")
+        return(str(date.today()))
 
 def getData(url = URL_ALL_DATA):
     """
@@ -63,6 +43,7 @@ def processDataIntoRegions(dataset = getData(), day = str(date.today())):
     Accepts a dataset, downloads it if not available
     Accepts a date to check, defaults to today
     """
+    print("Processing statistics for %s" % day)
     for values in dataset:
         if values['data'][:10] == day:
             if values['denominazione_regione'] == "P.A. Bolzano":
@@ -103,9 +84,9 @@ def printValues(dataset):
     for region, cases in dataset.items():
         print(region + ": " + str(cases))
 
-def writeToXlsx(dataset, date):
-    workbook = xlsxwriter.Workbook("Covid_data_" + date_to_check + ".xlsx")
-    worksheet = workbook.add_worksheet(date_to_check)
+def writeToXlsx(dataset, day):
+    workbook = xlsxwriter.Workbook("Covid_data_" + day + ".xlsx")
+    worksheet = workbook.add_worksheet(day)
 
     row = 0
     col = 0
@@ -121,9 +102,9 @@ def writeToXlsx(dataset, date):
 
     workbook.close()
 
-def writeToXls(dataset, date):
+def writeToXls(dataset, day):
     workbook = xlwt.Workbook(encoding = "utf-8")
-    worksheet = workbook.add_sheet(date_to_check)
+    worksheet = workbook.add_sheet(day)
 
     row = 0
     col = 0
@@ -137,7 +118,7 @@ def writeToXls(dataset, date):
         worksheet.write(row, col + 1, cases)
         row += 1
 
-    workbook.save("Covid_data_" + date_to_check + ".xls")
+    workbook.save("Covid_data_" + day + ".xls")
 
 
 # Before getting to the negative value solution above,
@@ -159,15 +140,49 @@ def writeToXls(dataset, date):
 #           reverse = True
 #       )   
 #   )
-    
 
-regions = processDataIntoRegions(getData(), date_to_check)
-if (len(regions.items())) > 0:
-    regions = sortRegions(regions)
-    printValues(regions)
-    if (args.xlsx):
-        writeToXlsx(regions, date_to_check)
-    if (args.xls):
-        writeToXls(regions, date_to_check)
-else:
-    print("No data available.")
+def main(args = None, day = str(date.today()), return_json = False):
+    
+    if (args):
+        if (args.date):
+            day = dateChecker(args.date)
+    else:
+        day = dateChecker(day)
+
+    regions = processDataIntoRegions(getData(), day)
+    
+    if (len(regions.items())) > 0:
+        regions = sortRegions(regions)
+        if not (return_json):
+            printValues(regions)
+        if (args):
+            if (args.xlsx):
+                writeToXlsx(regions, day)
+            if (args.xls):
+                writeToXls(regions, day)
+        if (return_json):
+            jsonfile = json.dumps(regions)
+            return jsonfile
+    else:
+        print("No data available for the day selected.")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Covid data digger')
+    parser.add_argument(
+        '--date',
+        type = str,
+        help = "Date you want to get the number of cases from, formatted AAAA-MM-DD"
+    )
+    parser.add_argument(
+        '--xlsx',
+        action='store_true',
+        help = "Write the results in an .xlsx file"
+    )
+    parser.add_argument(
+        '--xls',
+        action='store_true',
+        help = "Write the results in an .xls file"
+    )
+    args = parser.parse_args()
+
+    main(args)
